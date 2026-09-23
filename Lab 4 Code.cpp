55e0dd5adce9968049abff54a9b7683fe051ec32
mjudkins <mjudkins@cub.uca.edu>
2026-09-23 13:32:04 -0500

    Add files via upload

---
+++ Lab 4 Code.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+	std::string foodName1;
+	char itemCode1;
+	int itemQuantity1;
+	double unitPrice1;
+	bool isMember1;
+
+	std::string foodName2;
+	char itemCode2;
+	int itemQuantity2;
+	double unitPrice2;
+	bool isMember2;
+
+	std::string foodName3;
+	char itemCode3;
+	int itemQuantity3;
+	double unitPrice3;
+	bool isMember3;
+
+	foodName1 = "Coffee";
+	foodName2 = "Milk";
+	foodName3 = "Sugar";
+
+	std::cout << foodName1 << std::endl;
+	std::cout << foodName2 << std::endl;
+	std::cout << foodName3 << std::endl;
+
+
+
+
+}
